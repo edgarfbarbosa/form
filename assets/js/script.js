575c0046807dca4855ab8f6e84a8c4d01ec8741d
Edgar Faria Barbosa <edgarfbarbosa@outlook.com>
2024-02-07 22:28:15 -0300

    feat: adiciona validação para todos os campos do formulário

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,13 @@ const phone = document.getElementById('phone')
 const password = document.getElementById('password')
 const message = document.getElementById('message')
 
+function checkName(e) {
+  const value = e.target.value.trim()
+  const regex = /^[a-zA-Z\s]{2,}$/
+  
+  console.log(regex.test(value))
+}
+
 function checkEmail(e) {
   const value = e.target.value.trim()
   const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/
@@ -12,4 +19,27 @@ function checkEmail(e) {
   console.log(regex.test(value))
 }
 
+function checkPhone(e) {
+  const value = e.target.value.trim()
+  const regex = /^\d{11}$/
+  
+  console.log(regex.test(value))
+}
+
+function checkPassword(e) {
+  const value = e.target.value.trim()
+  
+  console.log(value.length)
+}
+
+function checkMessage(e) {
+  const value = e.target.value.trim()
+  
+  console.log(value.length)
+}
+
+name.addEventListener('change', checkName)
 email.addEventListener('change', checkEmail)
+phone.addEventListener('change', checkPhone)
+password.addEventListener('change', checkPassword)
+message.addEventListener('change', checkMessage)
