c2ba01745551893bc2d79c72ca24ce6ba27151d7
Edgar Faria Barbosa <edgarfbarbosa@outlook.com>
2024-02-13 12:13:48 -0300

    feat: adiciona lógica para exibir modal após validação

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,6 +44,23 @@ function handleCheckField(input, condition) {
   }
 }
 
+function showModal() {
+  const modal = document.getElementById('modal')
+  const btn = document.getElementById('btn')
+  
+  modal.style.display = 'block'
+  
+  btn.addEventListener('click', () => {
+    modal.style.display = 'none'
+  })
+  
+  function handleClickOut(e) {
+    if (e.target != modal) modal.style.display = 'none'
+  }
+  
+  window.addEventListener('click', handleClickOut)
+}
+
 function isValid(e) {
   e.preventDefault()
   
