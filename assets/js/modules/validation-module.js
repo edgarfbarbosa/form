907ab7e83e391e0b74be34bf914712e0380ea78f
Edgar Faria Barbosa <edgarfbarbosa@outlook.com>
2024-02-19 10:25:03 -0300

    feat: altera expressões regulares para validação de campos e adiciona comentário JSDoc nas funções is

--- assets/js/modules/validation-module.js
+++ assets/js/modules/validation-module.js
@@ -9,24 +9,49 @@ export default function initFormValidation() {
   const message = document.getElementById('message')
   const data = {}
   
+  /**
+  * Verifica se o nome fornecido é válido, deve conter apenas letras e espaços e no mínimo 2 caracteres.
+  * @param {string} name - O nome a ser verificado.
+  * @returns {boolean} Retorna verdadeiro se o nome for válido, falso caso contrário.
+  */
   function isName(name) {
-    return /^[a-zA-Z\s]{2,}$/.test(name)
+    return /^[a-zA-Z]{2,}(\s[a-zA-Z]+)*$/.test(name)
   }
   
+  /**
+  * Verifica se o e-mail fornecido é válido, seguindo o padrão comum de e-mail.
+  * @param {string} email - O e-mail a ser verificado.
+  * @returns {boolean} Retorna verdadeiro se o e-mail for válido, falso caso contrário.
+  */
   function isEmail(email) {
     return /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/.test(email)
   }
   
+  /**
+  * Verifica se o número fornecido é válido, seguindo o padrão brasileiro com DDD de dois dígitos e o restante do número.
+  * @param {string} phone - O número a ser verificado.
+  * @returns {boolean} Retorna verdadeiro se o número for válido, falso caso contrário.
+  */
   function isPhone(phone) {
-    return /^\d{11}$/.test(phone)
+    return /^\d{2}\d{8,9}$/.test(phone)
   }
   
+  /**
+  * Verifica se a senha fornecida é válida, seguindo os requisitos de ter entre 6 e 12 caracteres e incluir pelo menos uma letra maiúscula.
+  * @param {string} password - A senha a ser verificada.
+  * @returns {boolean} Retorna verdadeiro se a senha for válida, falso caso contrário.
+  */
   function isPassword(password) {
-    return password.length >= 8
+    return /^(?=.*[A-Z]).{6,12}$/.test(password)
   }
   
+  /**
+  * Verifica se a mensagem fornecida é válida, tendo pelo menos 10 caracteres.
+  * @param {string} message - A mensagem a ser verificada.
+  * @returns {boolean} Retorna verdadeiro se a mensagem for válida, falso caso contrário.
+  */
   function isMessage(message) {
-    return message.length >= 20
+    return /^[\s\S]{10,}$/.test(message)
   }
   
   function handleCheckField(input, condition) {
