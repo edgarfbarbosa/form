91ff879629fcd30f65abca4faba4ff4a85629a1d
Edgar Faria Barbosa <edgarfbarbosa@outlook.com>
2024-02-20 09:21:50 -0300

    docs: adiciona comentário JSDoc a função validateFieldAndStore

--- assets/js/modules/validation-module.js
+++ assets/js/modules/validation-module.js
@@ -53,17 +53,30 @@ export default function initFormValidation() {
     return /^[\s\S]{10,}$/.test(message)
   }
   
-  function handleCheckField(input, condition) {
+  /**
+  * Valida o campo de entrada do formulário e armazena seu valor no localStorage se for válido.
+  * Atualiza o ícone de status do campo conforme validade do valor inserido.
+  * @param {HTMLInputElement} input - Elemento de entrada a ser validado.
+  * @param {Function} condition - Função que verifica se o valor do campo é válido.
+  */
+  function validateFieldAndStore(input, condition) {
+    // Obtém o valor do campo de entrada, removendo espaços em branco no início e no fim
     const value = input.value.trim()
+    // Obtém o elemento seguinte ao campo de entrada
     const icon = input.nextElementSibling.classList
     
+    // Verifica se o valor do campo de entrada é válido usando a função de condição
     if (condition(value)) {
+      // Se o valor for válido, remove o ícone de erro e adiciona o ícone de sucesso
       icon.remove('bi-x-circle-fill')
       icon.add('bi-check-circle-fill')
+      // Armazena o valor válido no localStorage
       localStorage[input.id] = value
     } else {
+      // Se o valor não for válido, remove o ícone de sucesso e adiciona o ícone de erro
       icon.remove('bi-check-circle-fill')
       icon.add('bi-x-circle-fill')
+      // Remove o valor inválido do localStorage
       localStorage.removeItem(input.id)
     }
   }
@@ -80,7 +93,7 @@ export default function initFormValidation() {
     ]
     
     fields.forEach((field) => {
-      handleCheckField(field.input, field.condition)
+      validateFieldAndStore(field.input, field.condition)
     })
     
     const keys = Object.keys(localStorage)
@@ -102,10 +115,10 @@ export default function initFormValidation() {
   }
   
   window.addEventListener('load', setValues)
-  name.addEventListener('change', () => handleCheckField(name, isName))
-  email.addEventListener('change', () => handleCheckField(email, isEmail))
-  phone.addEventListener('change', () => handleCheckField(phone, isPhone))
-  password.addEventListener('change', () => handleCheckField(password, isPassword))
-  message.addEventListener('change', () => handleCheckField(message, isMessage))
+  name.addEventListener('change', () => validateFieldAndStore(name, isName))
+  email.addEventListener('change', () => validateFieldAndStore(email, isEmail))
+  phone.addEventListener('change', () => validateFieldAndStore(phone, isPhone))
+  password.addEventListener('change', () => validateFieldAndStore(password, isPassword))
+  message.addEventListener('change', () => validateFieldAndStore(message, isMessage))
   form.addEventListener('submit', validateForm)
 }
